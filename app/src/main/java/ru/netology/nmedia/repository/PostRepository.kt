190cdc5461931285aa05a8c828b8c12b9d9a36aa
Andrei Borisov <clinri@ya.ru>
2022-12-17 12:44:20 +0300

    init commit 02_threads

---
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
@@ -0,0 +1,10 @@
+package ru.netology.nmedia.repository
+
+import ru.netology.nmedia.dto.Post
+
+interface PostRepository {
+    fun getAll(): List<Post>
+    fun likeById(id: Long)
+    fun save(post: Post)
+    fun removeById(id: Long)
+}
