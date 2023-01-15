b13add9a04904a4964063f258b7709531ad0c85d
Andrei Borisov <clinri@ya.ru>
2023-01-15 13:26:18 +0300

    add config BuildConfig

--- app/src/main/java/ru/netology/nmedia/api/PostApiService.kt
+++ app/src/main/java/ru/netology/nmedia/api/PostApiService.kt
@@ -1,6 +1,7 @@
 package ru.netology.nmedia.api
 
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -10,12 +11,26 @@ import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Path
+import ru.netology.nmedia.BuildConfig
 import ru.netology.nmedia.dto.Post
 import java.util.concurrent.TimeUnit
 
-private const val BASE_URL = "http://10.0.2.2:9999/api/slow/"
+private const val BASE_URL = "${BuildConfig.BASE_URL}/api/slow/"
+
+private val logging = HttpLoggingInterceptor().apply {
+    if (BuildConfig.DEBUG) {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+}
 
 private val client = OkHttpClient.Builder()
+    .let{
+        if (BuildConfig.DEBUG){
+            it.addInterceptor(logging)
+        } else {
+            it
+        }
+    }
     .connectTimeout(30, TimeUnit.SECONDS)
     .build()
 
